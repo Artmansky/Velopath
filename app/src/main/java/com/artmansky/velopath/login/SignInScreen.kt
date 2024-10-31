61084bfc8d3c9a52d7e7bd5499262370251a556b
Artmansky <tomaszartmanski@gmail.com>
2024-10-31 17:51:35 +0100

    Icon added to log in screen

--- app/src/main/java/com/artmansky/velopath/login/SignInScreen.kt
+++ app/src/main/java/com/artmansky/velopath/login/SignInScreen.kt
@@ -1,17 +1,27 @@
 package com.artmansky.velopath.login
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.velopath.R
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -23,6 +33,7 @@ fun PrintSignInScreen(
     onSignInClick: () -> Unit
 ) {
     val context = LocalContext.current
+
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
             Toast.makeText(
@@ -36,11 +47,50 @@ fun PrintSignInScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(32.dp),
         contentAlignment = Alignment.Center
     ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "Sign in")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(220.dp)
+                    .background(Color.White, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.velopath_icon),
+                    contentDescription = "Velopath Icon",
+                    modifier = Modifier.size(180.dp)
+                )
+            }
+
+            Text(
+                text = "Welcome to:",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Text(
+                text = "Velopath",
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            Text(
+                text = "Please sign in using",
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Button(
+                onClick = onSignInClick,
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text(
+                    text = "Google",
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
         }
     }
 }
