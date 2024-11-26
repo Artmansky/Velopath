94cfb6fa3bed3ad2cf42785da90b42e22a4a2b1e
Artmansky <tomaszartmanski@gmail.com>
2024-11-26 15:47:53 +0100

    Move to current location button added

--- app/src/main/java/com/app/velopath/destinations/Home.kt
+++ app/src/main/java/com/app/velopath/destinations/Home.kt
@@ -1,22 +1,7 @@
 package com.app.velopath.destinations
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.app.velopath.mapsHandling.MapsHandling
 import kotlinx.serialization.Serializable
 
@@ -30,46 +15,5 @@ fun PrintHome(
     darkMode: Boolean,
     onClick: () -> Unit
 ) {
-    Column(modifier = modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.75f)
-        ) {
-            mapsHandler.PrintMap(darkMode = darkMode)
-            Box(
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    contentAlignment = Alignment.TopStart
-                ) {
-                    FloatingActionButton(
-                        onClick = onClick
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Menu",
-                            modifier = Modifier.size(32.dp)
-                        )
-                    }
-                }
-            }
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.25f),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Hello Home",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    }
+    mapsHandler.PrintMap(modifier = modifier, darkMode = darkMode, onClick = onClick)
 }
--- app/src/main/java/com/app/velopath/mapsHandling/MapsHandling.kt
+++ app/src/main/java/com/app/velopath/mapsHandling/MapsHandling.kt
@@ -7,19 +7,28 @@ import android.location.Location
 import android.location.LocationManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.mapbox.geojson.Point
@@ -31,6 +40,7 @@ import com.mapbox.maps.extension.compose.style.MapStyle
 import com.mapbox.maps.plugin.PuckBearing
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateOptions
 
 fun isLocationPermissionGranted(context: Context): Boolean {
     return ContextCompat.checkSelfPermission(
@@ -43,7 +53,7 @@ class MapsHandling(private val context: Context) {
     private var defaultLocation: Pair<Double, Double> = Pair(-73.977001, 40.728847)
 
     @Composable
-    fun PrintMap(darkMode: Boolean) {
+    fun PrintMap(modifier: Modifier, darkMode: Boolean, onClick: () -> Unit) {
         var hasPermission by remember { mutableStateOf(isLocationPermissionGranted(context)) }
 
         val permissionLauncher = rememberLauncherForActivityResult(
@@ -57,57 +67,116 @@ class MapsHandling(private val context: Context) {
             }
         }
 
-        ShowMap(darkMode, hasPermission)
+        ShowMap(modifier, darkMode, hasPermission, onClick)
     }
 
     @Composable
-    private fun ShowMap(darkMode: Boolean, hasPermission: Boolean) {
+    private fun ShowMap(
+        modifier: Modifier,
+        darkMode: Boolean,
+        hasPermission: Boolean,
+        onClick: () -> Unit
+    ) {
         val mapViewportState = rememberMapViewportState()
 
         val userLocation = remember {
             if (hasPermission) getCurrentLocation(context) else null
         } ?: defaultLocation
 
-        LaunchedEffect(userLocation) {
-            mapViewportState.setCameraOptions {
-                center(Point.fromLngLat(userLocation.second, userLocation.first))
-                zoom(15.0)
-                pitch(0.0)
-            }
+        //Poprawic wyswietlanie poczatkowej pozycji na mapie
+        mapViewportState.setCameraOptions {
+            center(Point.fromLngLat(userLocation.second, userLocation.first))
+            zoom(15.0)
+            pitch(0.0)
         }
 
-        MapboxMap(
-            modifier = Modifier.fillMaxSize(),
-            mapViewportState = mapViewportState,
-            style = {
-                if (darkMode) {
-                    MapStyle(style = Style.DARK)
-                } else {
-                    MapStyle(style = Style.LIGHT)
+        Column(modifier = modifier.fillMaxSize()) {
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(0.75f)
+            ) {
+                MapboxMap(
+                    modifier = modifier.fillMaxSize(),
+                    mapViewportState = mapViewportState,
+                    style = {
+                        if (darkMode) {
+                            MapStyle(style = Style.DARK)
+                        } else {
+                            MapStyle(style = Style.LIGHT)
+                        }
+                    }
+                ) {
+                    MapEffect(Unit) { mapView ->
+                        mapView.location.updateSettings {
+                            locationPuck = createDefault2DPuck(withBearing = true)
+
+                            enabled = hasPermission
+
+                            puckBearing = PuckBearing.COURSE
+
+                            puckBearingEnabled = hasPermission
+                        }
+                    }
+
+                }
+                Box(
+                    modifier = modifier
+                        .align(Alignment.TopStart)
+                ) {
+                    Box(
+                        modifier = modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.TopStart
+                    ) {
+                        FloatingActionButton(
+                            onClick = onClick
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menu",
+                                modifier = modifier.size(32.dp)
+                            )
+                        }
+                    }
+                }
+                Box(
+                    modifier = modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(16.dp)
+                ) {
+                    FloatingActionButton(
+                        onClick = {
+                            //Dodac sprawdzanie czy puck dostepny
+                            mapViewportState.transitionToFollowPuckState(
+                                FollowPuckViewportStateOptions.Builder()
+                                    .pitch(0.0)
+                                    .zoom(15.0)
+                                    .build()
+                            )
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "Follow Location",
+                            modifier = modifier.size(32.dp)
+                        )
+                    }
                 }
             }
-        ) {
-            MapEffect(Unit) { mapView ->
-                mapView.location.updateSettings {
-                    locationPuck = createDefault2DPuck(withBearing = true)
-
-                    enabled = hasPermission
-
-                    puckBearing = PuckBearing.COURSE
-
-                    puckBearingEnabled = hasPermission
-                }
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(0.25f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Hello Home",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
-        }
-
-        FloatingActionButton(
-            modifier = Modifier
-                .padding(16.dp),
-            onClick = {
-                mapViewportState.transitionToFollowPuckState()
-            }
-        ) {
-            Icon(Icons.Default.PlayArrow, contentDescription = "Move to Current Location")
         }
     }
 
